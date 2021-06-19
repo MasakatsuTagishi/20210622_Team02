90b9f952d8df3a025ff29daf5676e84d1dcfb7fb
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2021-06-19 23:23:16 +0900

    Initial Commit

---
+++ photoLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  photoLife
+//
+//  Created by 田岸将勝 on 2021/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ photoLife/photoLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoLifeApp.swift
+//  photoLife
+//
+//  Created by 田岸将勝 on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct photoLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
